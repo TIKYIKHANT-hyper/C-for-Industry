a5039d6ab69455a47754144cca3bd63c79b0058f
Default_String <tikyikhant2334@gmail.com>
2023-02-10 15:39:23 +0630

    day10 lesson

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
-#include "tkklib.h"
 #include <stdio.h>
-
-int main(int argc,char *argv[]){
-    int myarr[] = {1,3,34,45,12,23};
-    printf("%d", intarrlen(myarr));
+#include "tkklib.h"
+#define MAX 50
+struct student{
+    int id;
+    int age;
+    char name[MAX];
+};
+int main(int argc,char **argv){
+    struct student mgmg = {1,16,"MgMg"};
+    FILE *fout = NULL;
+    fopen_s(&fout,"D:\\CodeWithCLion\\studentdata.dat","wb+");
+    if(fout == NULL){
+        return 1;
+    }
+    fseek(fout,(mgmg.id - 1)* sizeof(struct student),SEEK_SET);
+    fwrite(&mgmg,sizeof (struct student),1,fout);
     return 0;
 }
