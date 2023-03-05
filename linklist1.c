8799bb5b7a3638830dae1189c4f7fac1059ec5fd
Default_String <tikyikhant2334@gmail.com>
2023-03-05 22:09:41 +0630

    linklist1 update

--- linklist1.c
+++ linklist1.c
@@ -8,15 +8,16 @@ struct node{
     struct node *next;
 };
 
-struct node *createnewnode(int data){
+struct node *createnewnode(int data,char *key){
     struct node *newnode = (struct node*)malloc(sizeof(struct node));
     newnode->data = data;
     newnode->next = NULL;
+    strcpy_t(key,newnode->specialkey);
     return newnode;
 }
 
-void insertnode(struct node **nodeheader,int data){
-    struct node *newnode = createnewnode(data);
+void insertnode(struct node **nodeheader,int data,char spkey[MAX]){
+    struct node *newnode = createnewnode(data,spkey);
     newnode->next = *nodeheader;
     *nodeheader = newnode;
 }
@@ -24,6 +25,7 @@ void insertnode(struct node **nodeheader,int data){
 void printlist(struct node *header){
     while(header != NULL){
         printf("%d\n",header->data);
+        printf("%s\n",header->specialkey);
         header = header->next;
     }
     printf("\n");
@@ -31,12 +33,31 @@ void printlist(struct node *header){
 int isEmpty(struct node *head){
     return head == NULL;
 }
+
+void appendnode(struct node **header,int data,char spkey[MAX]){
+    struct node *newptr = createnewnode(data,spkey);
+    if(newptr != NULL){
+        struct node *previousptr = NULL;//assume as start of the list
+        struct node *currentptr = *header;//argument pointer is assume as current pointer for start
+        while(currentptr != NULL && currentptr->next != NULL){
+            previousptr = currentptr;
+            currentptr = currentptr->next;
+        }
+        if(currentptr == NULL){
+            *header = newptr;
+        }else{
+            currentptr->next = newptr;
+        }
+    }
+    else{
+        printf("Error appending new nodes");
+    }
+}
 int main(int argc,char *argv[]){
     struct node* Node = NULL;
-    insertnode(&Node,1);
-    insertnode(&Node,2);
-    insertnode(&Node,3);
-    insertnode(&Node,4);
+    appendnode(&Node,1,"Ok");
+    appendnode(&Node,2,"Go");
+    insertnode(&Node,3,"Shuttle");
     printf("Linked Lists:\n");
     printlist(Node);
     return 0;
