825db9ffb9bf8378056d3946c12597fb2c93e124
Default_String <tikyikhant2334@gmail.com>
2023-03-06 16:46:17 +0630

    Not finished swaping function

--- linklist1.c
+++ linklist1.c
@@ -33,7 +33,11 @@ void printlist(struct node *header){
 int isEmpty(struct node *head){
     return head == NULL;
 }
-
+int swap(nodeptr *sptr,int order){
+    nodeptr *temptr = *sptr;
+    (*sptr) = (*sptr)->next;
+    (*sptr)->next = *temptr;
+}
 void appendnode(struct node **header,int data,char spkey[MAX]){
     struct node *newptr = createnewnode(data,spkey);
     if(newptr != NULL){
@@ -78,17 +82,34 @@ int delete(nodeptr *sptr,int res){
         return -1;//data for deleting not found
     }
 }
+int sortdata(nodeptr *sptr,int order){
+    nodeptr previous = *sptr;
+    nodeptr current = (*sptr)->next;
+    if(isEmpty(*sptr)){
+        return -1;//no data to sort
+    }
+    while(current != NULL && current->next != NULL){
+        if(previous->data > current->data ){
+            //swap(previous,current,current->next);
+        }
+    }
+    return 0;
+}
+void insert(nodeptr *sptr,int data,char *key){
+
+}
 int main(int argc,char *argv[]){
     struct node* Node = NULL;
     appendnode(&Node,1,"Ok");
     appendnode(&Node,2,"Go");
     insertnode(&Node,3,"Shuttle");
+    appendnode(&Node,4,"Lamp");
     printf("Linked Lists:\n");
     printlist(Node);
-    printf("after deleting\n");
-    delete(&Node,2);
+    swap(Node,1);
     printlist(Node);
     return 0;
-}//
+}
+//
 // Created by Ti Kyi Khant on 3/5/2023.
 //
