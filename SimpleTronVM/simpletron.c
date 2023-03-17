4ebb959ddf20df103f3912ffe577558f13424244
Default_String <tikyikhant2334@gmail.com>
2023-03-17 23:55:11 +0630

    simpletron

--- SimpleTronVM/simpletron.c
+++ SimpleTronVM/simpletron.c
@@ -1,6 +1,14 @@
 //
 // Created by Ti Kyi Khant on 3/17/2023.
 //
+
+#include <stdio.h>
+#include <stdint.h>
+#include <signal.h>
+/* windows only */
+#include <Windows.h>
+#include <conio.h>
+
 //Memory Storage
 #define MEMORY_MAX (1 << 16)
 unsigned short memory[MEMORY_MAX];
@@ -18,7 +26,7 @@ enum
     R_R7,
     R_PC,//next instruction address in memory
     R_COND,//flag register, indicating previous instruction
-    R_COUNT//
+    R_COUNT
 };
 
 unsigned short reg[R_COUNT];
@@ -35,8 +43,25 @@ enum
     OP_LD,//LOAD
     OP_ST,//STORE
     OP_JSR,//JUMP REGISTER
-    OP_AND,//BIT WISE AND
+    OP_AND,//BITWISE AND
     OP_LDR,//LOAD REGISTER
     OP_SDR,//STORE REGISTER
     OP_RTI,//UNUSED
+    OP_NOT,//BITWISE NOT
+    OP_LDI,//LOAD INDIRECT
+    OP_STI,//STORE INDIRECT
+    OP_JMP,//JUMP
+    OP_RES,//RESERVED(UNUSED)
+    OP_LEA,//LOAD EFFECTIVE ADDRESS
+    OP_TRAP//EXECUTE TRAP
 };
+
+//condition flags
+
+enum
+{
+    FL_POS = 1 << 0,//P
+    FL_ZRO = 1 << 1,//Z
+    FL_NEG = 1 << 2,//N
+};
+
