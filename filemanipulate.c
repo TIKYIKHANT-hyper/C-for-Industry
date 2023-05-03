abbc71404bda7e7d4751e3126e5992127ae29601
Default_String <tikyikhant2334@gmail.com>
2023-05-03 21:44:16 +0630

    reading from file

--- filemanipulate.c
+++ filemanipulate.c
@@ -1,3 +1,27 @@
 //
 // Created by user on 5/3/23.
 //
+#include <stdio.h>
+
+int main(){
+    char name[20];
+    int id;
+    double balance;
+
+    FILE *cfPtr;
+    if((cfPtr = fopen("/home/user/C-for-Industry/clients.dat","w"))== NULL){
+        printf("cannot open file\n");
+    }
+    else{
+        printf("Enter the account,name and balance:\n");
+        printf("Enter EOF to exit\n");
+        scanf("%d%s%lf", &id, name, &balance);
+
+        while(!feof(stdin)){
+            fprintf(cfPtr,"%d %s %.2f\n",id,name,balance);
+            scanf("%d%s%lf", &id, name, &balance);
+        }
+        fclose(cfPtr);
+    }
+    return 0;
+}
