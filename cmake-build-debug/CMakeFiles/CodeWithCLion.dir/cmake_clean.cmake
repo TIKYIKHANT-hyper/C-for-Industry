096b2607403c824d68854bf5d0cbd9d3827a6119
Default_String <tikyikhant2334@gmail.com>
2023-05-03 19:09:53 +0630

    file manipulate

---
+++ cmake-build-debug/CMakeFiles/CodeWithCLion.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CodeWithCLion.dir/main.c.o"
+  "CMakeFiles/CodeWithCLion.dir/main.c.o.d"
+  "CodeWithCLion"
+  "CodeWithCLion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CodeWithCLion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.c
+++ main.c
@@ -1,55 +1,24 @@
 #include <stdio.h>
-#include <conio.h>
-#include <stdlib.h>
 
-int main()
-{
-    int x = 10, y = 10; // Starting position of the player
-    char key;
+int main(){
+    char name[20];
+    int id;
+    float balance;
 
-    while (1) // Infinite loop to keep the game running
-    {
-        system("cls"); // Clear the console screen
-
-        // Draw the player at their current position
-        for (int i = 0; i < y; i++)
-        {
-            printf("\n");
-        }
-        for (int i = 0; i < x; i++)
-        {
-            printf(" ");
-        }
-        printf("O\n");
-
-        // Get user input
-        if (kbhit()) // Check if a key has been pressed
-        {
-            key = getch(); // Get the key that was pressed
-
-            // Move the player based on the key that was pressed
-            switch (key)
-            {
-                case 'w':
-                    y--;
-                    break;
-                case 'a':
-                    x--;
-                    break;
-                case 's':
-                    y++;
-                    break;
-                case 'd':
-                    x++;
-                    break;
-            }
-        }
-
-        // Wait for a short period of time before redrawing the player
-        // to prevent flickering
-        for (int i = 0; i < 1000000; i++);
-
+    FILE *cfPtr;
+    if((cfPtr = fopen("/home/user/C-for-Industry/clients.dat","w"))== NULL){
+        printf("cannot open file\n");
     }
+    else{
+        printf("Enter the account,name and balance:\n");
+        printf("Enter EOF to exit\n");
+        scanf("%d%s%lf", &id,&name,&balance);
 
+        while(!feof(stdin)){
+            fprintf(cfPtr,"%d %s %.2f\n",id,name,balance);
+            scanf("%d%s%lf", &id, &name, &balance);
+        }
+        fclose(cfPtr);
+    }
     return 0;
 }
